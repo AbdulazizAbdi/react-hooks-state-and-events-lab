450a5c2403fb59519eba37704fe0164d1d7ae2ce
Abdulaziz Abdi <amigf76@gmail.com>
2024-04-27 10:03:08 +0300

    state an events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  
+  const [itemState, setItemState] = useState(false)
+  const itemStatus = itemState ? "in-cart" : ""
+  const itemTextStatus = itemState ? "Remove from cart" : "Add to Cart"
+
+  function handleItem() {
+    setItemState(!itemState)
+  }
+  
   return (
-    <li className="">
+    <li className={itemStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleItem}>{itemTextStatus}</button>
     </li>
   );
 }
